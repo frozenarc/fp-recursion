6a82a34130cb2e1a014886b01066984d47addc25
Manan Panchal <mananvpanchal@gmail.com>
2018-07-02 19:52:22 +0530

    formatting file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function forEach(sArr, tArr, opr, idx = 0) {
-	if(idx === sArr.length - 1) {
-		return opr(sArr[idx], tArr, idx, sArr);
-	} else {
-		return forEach(sArr, opr(sArr[idx], tArr, idx, sArr), opr, idx + 1);
-	}
+    if (idx === sArr.length - 1) {
+        return opr(sArr[idx], tArr, idx, sArr);
+    } else {
+        return forEach(sArr, opr(sArr[idx], tArr, idx, sArr), opr, idx + 1);
+    }
 };
 
 function forLoop(start, end, sArr, opr) {
-    if(start === end - 1) {
+    if (start === end - 1) {
         return opr(sArr, start, end);
     } else {
         return forLoop(start + 1, end, opr(sArr, start, end), opr);
