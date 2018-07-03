c5bd7bcaa393a110e9060cf12acec1965b1f5bb1
Manan Panchal <mananvpanchal@gmail.com>
2018-07-03 16:47:51 +0530

    code optimized

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 function forEach(src, target, opr, idx = 0, incr = 1) {
-    if (idx >= src.length - incr) {
-        return opr(src[idx], target, idx, src);
-    } else {
-        return forEach(src, opr(src[idx], target, idx, src), opr, idx + incr, incr);
-    }
+    const newTarget = opr(src[idx], target, idx, src);
+    return idx >= src.length - incr
+        ? newTarget
+        : forEach(src, newTarget, opr, idx + incr, incr);
 };
 
 function forLoop(count, target, opr, idx = 0, incr = 1) {
-    if (idx >= count - incr) {
-        return opr(idx, target, count);
-    } else {
-        return forLoop(count, opr(idx, target, count), opr, idx + incr, incr);
-    }
+    const newTarget = opr(idx, target, count);
+    return idx >= count - incr
+        ? newTarget
+        : forLoop(count, newTarget, opr, idx + incr, incr);
 };
 
 exports.forEach = forEach;
