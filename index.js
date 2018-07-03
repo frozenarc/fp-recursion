e82645678cb791f30fb518d47c0a76c928e57ed7
Manan Panchal <mananvpanchal@gmail.com>
2018-07-04 00:41:49 +0530

    recurFor and recurTill

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-function recurEach(src, target, opr, idx = 0, incr = 1) {
+function _recurFor(src, target, opr, idx, incr) {
     const newTarget = opr(src[idx], target, idx, src);
     return idx >= src.length - incr
         ? newTarget
-        : recurEach(src, newTarget, opr, idx + incr, incr)
+        : _recurFor(src, newTarget, opr, idx + incr, incr)
 }
 
-function forEach(src, idx = 0, incr = 1) {
+function recurFor(src, idx = 0, incr = 1) {
     return {
         opr: (opr, target) => {
             return src.length === 0
                 ? target
-                : recurEach(src, target, opr, idx, incr);
+                : _recurFor(src, target, opr, idx, incr);
         }
     };
 };
 
-function forLoop(count, target, opr, idx = 0, incr = 1) {
-    if (count === 0) {
-        return target;
-    } else {
-        const newTarget = opr(idx, target, count);
-        return idx >= count - incr
-            ? newTarget
-            : forLoop(count, newTarget, opr, idx + incr, incr);
-    }
+function _recurTill(count, target, opr, idx, incr) {
+    const newTarget = opr(idx, target, count);
+    return idx >= count - incr
+        ? newTarget
+        : _recurTill(count, newTarget, opr, idx + incr, incr);
+}
+
+function recurTill(count, idx = 0, incr = 1) {
+    return {
+        opr: (opr, target) => {
+            return count === 0 
+            ? target
+            : _recurTill(count, target, opr, idx, incr);
+        }
+    };
 };
 
-exports.forEach = forEach;
-exports.forLoop = forLoop;
+exports.recurFor = recurFor;
+exports.recurTill = recurTill;
--- test.js
+++ test.js
@@ -1,120 +1,101 @@
-const { forEach, forLoop } = require('./index');
+const { recurFor, recurTill } = require('./index');
 const expect = require('chai').expect;
 
-describe('forEach', () => {
+describe('recurFor', () => {
 
     it('Array', () => {
-        expect(
-            forEach([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                .opr((val, arr = []) => [...arr, val + 1])
-        ).to.deep.equal([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
-    });
+        const res = recurFor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            .opr((val, arr = []) => [...arr, val + 1]);
 
-    /*it('Array', () => {
-        expect(forEach(
-            [], 
-            [], 
-            (val, arr) => [...arr, val + 1]
-        )).to.deep.equal([]);
+        expect(res).to.deep.equal([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
     });
 
     it('Array', () => {
-        expect(forEach(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
-            [], 
-            (val, arr) => [...arr, val + 1]
-        )).to.deep.equal([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+        const res = recurFor([])
+            .opr((val, arr) => [...arr, val + 1], []);
+
+        expect(res).to.deep.equal([]);
+    });
+
+    it('Array', () => {
+        const res = recurFor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            .opr((val, arr = []) => [...arr, val + 1]);
+
+        expect(res).to.deep.equal([2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
     });
 
     it('Array, idx = 3', () => {
-        expect(forEach(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
-            [], 
-            (val, arr) => [...arr, val + 1], 
-            3,
-            1
-        )).to.deep.equal([5, 6, 7, 8, 9, 10, 11]);
+        const res = recurFor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
+            .opr((val, arr = []) => [...arr, val + 1]);
+
+        expect(res).to.deep.equal([5, 6, 7, 8, 9, 10, 11]);
     });
 
     it('Array, idx = 0, incr = 2', () => {
-        expect(forEach(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 
-            [], 
-            (val, arr) => [...arr, val + 1], 
-            0,
-            2
-        )).to.deep.equal([2, 4, 6, 8, 10, 12]);
+        const res = recurFor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0, 2)
+            .opr((val, arr = []) => [...arr, val + 1]);
+
+        expect(res).to.deep.equal([2, 4, 6, 8, 10, 12]);
     });
 
     it('Custom Object', () => {
-        expect(forEach(
-            [1, 2, 3], 
-            {}, 
-            (val, obj) => ({ prev: Object.assign({}, obj), cur: val})
-        )).to.deep.equal({ prev: { prev: { prev: {}, cur: 1 }, cur: 2 }, cur: 3 });
+        const res = recurFor([1, 2, 3])
+            .opr((val, obj = {}) => ({ prev: Object.assign({}, obj), cur: val }));
+
+        expect(res).to.deep.equal({ prev: { prev: { prev: {}, cur: 1 }, cur: 2 }, cur: 3 });
     });
 
     it('Primitive Value', () => {
         //4 + 1 + 2 + 3
-        expect(forEach(
-            [1, 2, 3], 
-            4, 
-            (val, prim) => prim + val
-        )).to.equal(10);
-    });*/
+        const res = recurFor([1, 2, 3])
+            .opr((val, prim = 4) => prim + val);
+
+        expect(res).to.equal(10);
+    });
 });
 
 describe('forLoop', () => {
 
     it('Array', () => {
-        expect(forLoop(
-            0,
-            [],
-            (idx, arr) => [...arr, idx * 2]
-        )).to.deep.equal([]);
+        const res = recurTill(0)
+            .opr((idx, arr) => [...arr, idx * 2], []);
+
+        expect(res).to.deep.equal([]);
     });
 
     it('Array', () => {
-        expect(forLoop(
-            10,
-            [],
-            (idx, arr) => [...arr, idx * 2]
-        )).to.deep.equal([0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
+        const res = recurTill(10)
+            .opr((idx, arr = []) => [...arr, idx * 2]);
+
+        expect(res).to.deep.equal([0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
     });
 
     it('Array, idx = 3', () => {
-        expect(forLoop(
-            10,
-            [],
-            (idx, arr) => [...arr, idx * 2],
-            3
-        )).to.deep.equal([6, 8, 10, 12, 14, 16, 18]);
+        const res = recurTill(10, 3)
+            .opr((idx, arr = []) => [...arr, idx * 2]);
+
+        expect(res).to.deep.equal([6, 8, 10, 12, 14, 16, 18]);
     });
 
     it('Array, idx = 0, incr = 2', () => {
-        expect(forLoop(
-            10,
-            [],
-            (idx, arr) => [...arr, idx * 2],
-            0,
-            2
-        )).to.deep.equal([0, 4, 8, 12, 16]);
+        const res = recurTill(10, 0, 2)
+            .opr((idx, arr = []) => [...arr, idx * 2]);
+
+        expect(res).to.deep.equal([0, 4, 8, 12, 16]);
     });
 
     it('Custom Object', () => {
-        expect(forLoop(
-            3,
-            {},
-            (idx, obj) => ({ prev: Object.assign({}, obj), cur: idx })
-        )).to.deep.equal({ prev: { prev: { prev: {}, cur: 0 }, cur: 1 }, cur: 2 });
+        const res = recurTill(3)
+            .opr((idx, obj = {}) => ({ prev: Object.assign({}, obj), cur: idx }));
+
+        expect(res).to.deep.equal({ prev: { prev: { prev: {}, cur: 0 }, cur: 1 }, cur: 2 });
     });
 
     it('Primitive Value', () => {
         //4 + 0 + 1 + 2 + 3 + 4
-        expect(forLoop(
-            5,
-            4,
-            (idx, prim) => prim + idx
-        )).to.equal(14);
+        const res = recurTill(5)
+            .opr((idx, prim = 4) => prim + idx);
+
+        expect(res).to.equal(14);
     });
 });
